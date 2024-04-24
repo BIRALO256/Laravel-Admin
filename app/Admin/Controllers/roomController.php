5920509a3f94442a90374382ba1aa82c179392ce
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-04-24 17:50:31 +0300

    played with basic forms

--- app/Admin/Controllers/roomController.php
+++ app/Admin/Controllers/roomController.php
@@ -15,7 +15,7 @@ class roomController extends AdminController
      *
      * @var string
      */
-    protected $title = 'room';
+    protected $title = 'Room';
 
     /**
      * Make a grid builder.
@@ -26,9 +26,9 @@ class roomController extends AdminController
     {
         $grid = new Grid(new room());
 
-        $grid->column('id', __('Id'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        // $grid->column('id', __('Id'));
+        // $grid->column('created_at', __('Created at'));
+        // $grid->column('updated_at', __('Updated at'));
         $grid->column('name', __('Name'));
         $grid->column('avaliability', __('Avaliability'));
         $grid->column('price', __('Price'));
@@ -74,6 +74,23 @@ class roomController extends AdminController
         $form->number('price', __('Price'));
         $form->textarea('services', __('Services'));
         $form->textarea('details', __('Details'));
+
+        $form->tools(function (Form\Tools $tools) {
+
+            // Disable `List` btn.
+            // $tools->disableList();
+        
+            // Disable `Delete` btn.
+            $tools->disableDelete();
+        
+            // Disable `Veiw` btn.
+            // $tools->disableView();
+        
+            // Add a button, the argument can be a string, or an instance of the object that implements the Renderable or Htmlable interface
+            // $tools->add('<a class="btn btn-sm btn-danger"><i class="fa fa-trash"></i>&nbsp;&nbsp;delete</a>');
+        });
+        
+
 
         return $form;
     }
