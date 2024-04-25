1da57f5c6427ea9253f0702c9139ef52a04e208e
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-04-25 13:53:08 +0300

    i forgot to commit yesteday

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,5 +13,6 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('home');
     $router->resource('rooms', roomController::class);
+    $router->resource('buildings', buildingController::class);
 
 });
